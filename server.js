eabf8e401913cf233ae2f37bc78a0b39bf0817c1
Shakti-Singh007 <shakti.singh@pizoneinfotech.com>
2024-10-07 11:10:51 +0530

    fixed cors error

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 const db = require('./config/db'); // Importing to ensure DB is initialized
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use('/api', userRoutes);
