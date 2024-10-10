88604435a380d279d5a75f2b2baaa3dee38ab90c
Shakti-Singh007 <shakti.singh@pizoneinfotech.com>
2024-10-10 15:43:38 +0530

    fixed login apies

--- user_modules/helpers/validetionPasswprd.js
+++ user_modules/helpers/validetionPasswprd.js
@@ -1,12 +1,12 @@
-const { body, validationResult } = require('express-validator');
+const Joi = require('joi');
 
-exports.forgetPassword = [
-    body('email').isString().notEmpty().withMessage('ShopCode is required'),
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        next();
-    }
-  ];
+// Aggregator validation schema
+const userSchema = Joi.object({
+    email: Joi.string().max(255).required(),
+  password:Joi.string().max(255).required(),
+
+});
+
+exports.validateUser = (user) => {
+    return userSchema.validate(user);
+};
--- user_modules/routes.js
+++ user_modules/routes.js
@@ -6,19 +6,18 @@ const User = require('./controllers/userController')
 const addRoutes = (app, router) => {
 
   router.post('/auth/login',
-    validateUser.validateUser,
    User.loginUser
    );
  router.post('/auth/forgetPassword',
-  validateUser.forgetPassword ,
+
     User.forgetPassword
     );
    router.post('/auth/verified-otp',
-    validateUser.verificationOtp ,
+
     User.verifiedOtp
     );
  router.post('/auth/reset-password',
-  validateUser.resetPassword,
+
       User.resetPassword
       );
 
